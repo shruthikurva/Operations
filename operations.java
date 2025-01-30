b962b94b7f2861938e6d5b6ec53d6b5af50924cc
shruthikurva <kurvashruthi98@gmail.com>
2025-01-30 21:46:58 +0530

    Add files via upload

---
+++ operations.java
@@ -0,0 +1,19 @@
+public class operations {
+    public static void main(String[] args) {
+        int a=63;
+        int b=54;
+        //addition
+        int sum=a+b;
+        System.out.println("sum="+sum);
+        //sudtraction
+        int difference=a-b;
+        System.out.println("difference="+difference);
+        //multiplication
+        int product=a*b;
+        System.out.println("Product="+product);
+        //Division
+        int quotient=a/b;
+        System.out.println("quotient="+quotient);
+
+    }
+}
